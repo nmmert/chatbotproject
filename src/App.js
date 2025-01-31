5ad866dae5d071ae543adabc4d470b56e331937f
nmmert <murat.mert@std.yildiz.edu.tr>
2025-01-31 13:34:07 +0300

    Image update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Chatbot from "./Chatbot";
+import coffee_background from "./assets/coffee-background3.jpg";
 
 function App(){
   return(
-    <div className="App">
+    <div className="App" style={{ backgroundImage: `url(${coffee_background})`, backgroundSize: "cover", height: "100vh" }}>
       <h1>Smart Coffee Assistant</h1>
       <Chatbot/>
     </div>
